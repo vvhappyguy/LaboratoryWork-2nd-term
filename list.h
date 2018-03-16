c2fd71f6ee5d2e3e2f3777db270ffb7df9834467
vvhappyguy <vvhappyguy@gmail.com>
2018-03-17 00:10:16 +0300

    Added notes-questions for 17.02 lesson

--- list.h
+++ list.h
@@ -15,23 +15,23 @@ class List{
 		class iterator{
 				Node* current; //Current Node with two links and value
 			public:
-				void next();//
-				void prev();
-				bool valid() const;
-				int& get();
-				int get() const;
+				void next(); //Why it returned void?
+				void prev(); //Why it returned void? Maybe it may return iterator*
+				bool valid() const; //Checking for iterator life
+				int& get(); //get wr-method
+				int get() const; //get only-read method
 				bool equal(/*const interator*/) const; //Compare two iterators - MISTAKE HERE
 		};
 
 		void push_back(); //Push to tail of List new element
 		iterator begin(); //Link for List's begin
 		iterator end(); //Link for List's end
-		iterator last(); 
-		iterator before(); 
-		size_t size() const; 
-		bool empty() const; 
+		iterator last(); //Link to next-end
+		iterator before(); //Link to prev.begin 
+		size_t size() const; //size of List
+		bool empty() const; //checking for list isn't empty
 		//<c-tors>
 		//<d-tors>
-		void insert(iterator, int);
-		void erase(iterator);
+		void insert(iterator, int); //insert new iterator to "int" position
+		void erase(iterator); //remove current iterator from List
 };
